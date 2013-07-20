23dba89edc8d0e4ba6a8e2fd42a27710e56de2bb
Mark Roberts <markandrusroberts@gmail.com>
2013-07-19 18:18:53 -0700

    Fixed a bug

--- routes/Twilio.js
+++ routes/Twilio.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
 var twilio = require('twilio');
 
 app.post('/', function(req, res) {
-  if (key = req.body.key &&
+  if ((key = req.body.key) &&
       twilio.validateExpressRequest(req, process.env.TWILIO_AUTH_TOKEN))
     res.redirect(302, '/TwiML/' + key);
   else
