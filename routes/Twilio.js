9cc44e91209aab25fe095ff91fa95ae04a330f9e
Mark Roberts <markandrusroberts@gmail.com>
2013-07-19 18:24:06 -0700

    Fixed paths

--- routes/Twilio.js
+++ routes/Twilio.js
@@ -2,13 +2,16 @@ module.exports = function(app) {
 
 var twilio = require('twilio');
 
-app.post('/', function(req, res) {
+function handle(req, res) {
   if ((key = req.body.key) &&
       twilio.validateExpressRequest(req, process.env.TWILIO_AUTH_TOKEN))
     res.redirect(302, '/TwiML/' + key);
   else
     res.send(400);
   res.end();
-});
+}
+
+app.post('', handle);
+app.post('/', handle);
 
 };
