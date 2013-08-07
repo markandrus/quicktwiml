887559d42f5956ed36a51de5b7bdf5cc121160f7
Mark Roberts <markandrusroberts@gmail.com>
2013-08-07 14:29:12 -0700

    Update embed route in app.js

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 
 require('./routes/token-js.js')(app, express.basicAuth);
 require('./routes/client-js.js')(app);
+require('./routes/embed/load-js.js')(app);
 require('./routes/validate.js')(app);
 require('./routes/TwiML.js')(app, models.TwiML);
 require('./routes/Twilio.js')(app);
