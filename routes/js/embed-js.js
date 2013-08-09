ef4d0d03c85ffcc14ee21db14762d5aad501e338
Mark Roberts <markandrusroberts@gmail.com>
2013-08-09 12:11:14 -0700

    Bugfix

--- routes/js/embed-js.js
+++ routes/js/embed-js.js
@@ -2,16 +2,13 @@ module.exports = function(app) {
 
 app.get('/js/embed.js', function(req, res) {
   res.set('Content-Type', 'application/javascript');
-  if ('key' in req.query)
-    return res.render('js/embed-js', {
-      key: req.query.key,
-      // FIXME: ...
-      appDomain: process.env.APP_DOMAIN || '127.0.0.1:3000',
-      appDomainRegex:
-        (process.env.APP_DOMAIN || '127.0.0.1:3000').replace(/./, '\.')
-    });
-  res.send('');
-  res.end();
+  res.render('js/embed-js', {
+    key: req.query.key,
+    // FIXME: ...
+    appDomain: process.env.APP_DOMAIN || '127.0.0.1:3000',
+    appDomainRegex:
+      (process.env.APP_DOMAIN || '127.0.0.1:3000').replace(/./, '\.')
+  });
 });
 
 };
